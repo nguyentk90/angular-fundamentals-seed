3d9432c6c7c58e7532ac6d251258a16c4a18b371
Tran Khanh Nguyen <nguyentk90@outlook.com>
2017-02-15 21:53:29 +0700

    code component

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,13 +5,14 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      {{title}}
+      {{ title }}
     </div>
   `
 })
 export class AppComponent {
   title: string;
+
   constructor() {
-    this.title = 'Ultimate Angular!';
+    this.title = 'Ultimate Angular'
   }
 }
