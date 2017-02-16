32db80ace1f90a095b149cd294846d99ca0afc09
Tran Khanh Nguyen <nguyentk90@outlook.com>
2017-02-16 08:47:50 +0700

    data binding code

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,14 +5,52 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      {{ title }}
+      <h1 [innerHTML]="title"></h1>
+      <div>
+        {{numberOne + numberTwo}}
+      </div>
+      <div>
+        {{isHappy ? ':)' : '('}}
+      </div>
+      <input type="text"
+        [ngModel]="name"
+        (ngModelChange)="handleChange($event)"
+        (blur)="handleBlur($event)"
+        (input)="handleInput($event)"
+         />
+      <div>{{name}}</div>
+      <button (click)="handleClick($event)">Change name</button>
     </div>
   `
 })
 export class AppComponent {
   title: string;
+  isHappy = true;
+  numberOne: number = 1;
+  numberTwo: number = 2;
+  name: string = 'Nguyen';
 
   constructor() {
     this.title = 'Ultimate Angular'
+  }
+
+  handleChange(value: string) {
+    console.log('Handle change...');
+    this.name = value;
+  }
+
+  handleClick() {
+    console.log('Handle click...');
+    this.name = 'Tran';
+  }
+
+  handleBlur(event: any) {
+    this.name = event.target.value; 
+    console.log('Handle blur...');
+  }
+
+  handleInput(event: any) {
+    console.log('Handle input...');
+    this.name = event.target.value;
   }
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   imports: [
     BrowserModule,
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   declarations:  [
     AppComponent
